b402132cedd4381b7badef7cbb05357a0acd7ac3
MFrandDK <94894199+MFrandDK@users.noreply.github.com>
2023-06-15 17:34:59 +0200

    test config

--- Konditorkunsten/src/router/index.js
+++ Konditorkunsten/src/router/index.js
@@ -9,7 +9,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: 'https://mmd.ucn.dk/class/MMD-CSD-S21/10407721/Sem4/dist/',
       name: 'shop',
       component: ShopView
     },
